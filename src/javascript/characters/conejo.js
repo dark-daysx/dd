237cb26703874ca1c199798bbf7638d0f1b5b21e
Yadiel Shamdee <yadielshamdee@gmail.com>
2020-08-27 08:03:17 -0500

    😳

--- src/javascript/characters/conejo.js
+++ src/javascript/characters/conejo.js
@@ -1,7 +1,7 @@
 setTimeout(() => {
     var i = 0;
 
-    var txt = `Biografia De Conejo`;
+    var txt = `Mi padre siempre ha dicho que mi familia es algo especial, que es diferente a las demas familias. Siempre me pregunte el porque, el dice que es porque seremos un antes y un despues... literalmente, siempre nos dice eso a mi y a mi hermana kopy, pero nunca lo tomamos como algo serio. Soy Conejo! estudio, practico natacion, me gusta crear mundos fantasiosos junto con mi hermana y imaginar que se cumplen... el sueño de toda princesa... incluso tengo dos guardias reales, mis amigos; harian por mi lo que fuera! mi vida... mi vida no sera la mejor, pero estoy feliz de tenerla.`;
     var element = 'conejoS'
     var speed = 50;
     
