7edefb5157f26189741fea842c46645761f22779
dark-daysx <63689450+dark-daysx@users.noreply.github.com>
2021-03-09 19:35:07 -0500

    Update count.js

--- src/javascript/count.js
+++ src/javascript/count.js
@@ -27,7 +27,7 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById(element).innerHTML = "EXPIRED";
+    document.getElementById(element).innerHTML = "CANCELLED <3";
   }
 }, 1000);
 }
